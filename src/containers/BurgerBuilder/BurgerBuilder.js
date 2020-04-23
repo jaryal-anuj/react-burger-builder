27f7345639d94ccc96a8b32f62ff354bacc962da
jaryal-anuj <jariyal.anuj@gmail.com>
2020-04-23 04:35:50 -0700

    form validation

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,29 +87,19 @@ class BurgerBuilder  extends Component {
     }
 
     purchaseContinueHandler=()=>{
-        this.setState({ loading:true });
-        const order ={
-            ingredients:this.state.ingredients,
-            price:this.state.totalPrice,
-            customer:{
-                name:"anuj",
-                address:{
-                    street:'sdf',
-                    zipCode:'2343',
-                    country:'India'
-                },
-                email:'adf@klj.com'
-            },
-            diliveryMethod:'fastest'
+     
+        const queryParams = [];
+        for(let i in this.state.ingredients){
+            queryParams.push(encodeURIComponent(i) + '=' +encodeURIComponent(this.state.ingredients[i]));
+
         }
-        axios.post('/orders.json',order)
-            .then(response=>{
-                this.setState({ loading:false,purchasing:false });
-            })
-            .catch(error=>{
-                console.log(error);
-                this.setState({ loading:false, purchasing:false });
-            });
+        queryParams.push('price='+this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname:'/checkout',
+            search:'?'+queryString
+
+        });
     }
 
     render(){
